89a1142baf1bd08d293520ace8775603dcfdb70a
Anurag5 <anuraghere555@gmail.com>
2025-07-09 20:41:54 +0530

    Add files via upload

---
+++ C++:oops/33.polymorphism.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  //polymorphism->poly means many and morphism--> forms(shapes,ways)
+  // so basically -->(one name and multiple form)or eutai chiz ko multiple forms
+  //Compile time POLYMphsm-->fn overloading,operator overloading,
+  //Runtime POLYMphsm--> virtual fn
+
+// Fn overloading and Operator overloading done already
+// virtual fn we'll do now
+  return 0;
+}
