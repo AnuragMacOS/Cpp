580d33fbf5634e9df5700b1ae1e30dd4ff5e5665
Anurag5 <anuraghere555@gmail.com>
2025-11-22 23:51:34 +0530

    Add files via upload

---
+++ Arrays/nestedforloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+for (int i=1;i<=3;i++) //i is for rows
+{
+  for(int j=1;j<=5;j++) //j is for columns
+  {
+    cout<<"("<<i<<","<<j<< ") ";
+  }
+  cout<<endl;
+}
+
+
+  return 0;
+}
