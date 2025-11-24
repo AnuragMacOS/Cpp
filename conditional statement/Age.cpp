6bf51ce30c6f82e5c1f61ab4e6f713653f10f5d3
Anurag5 <anuraghere555@gmail.com>
2025-11-24 23:03:25 +0530

    Add files via upload

---
+++ conditional statement/Age.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int age;
+  cout<<"Enter age:";
+  cin>>age;
+   if(age>=12 && age<=40){
+     cout<<"He is young";
+   }
+   else{
+    cout<<"He isn't young ";
+   }
+   return 0;
+
+}
