f7d7386db25d807e46f9f11efae57fcbc1c2a48d
Anurag5 <anuraghere555@gmail.com>
2025-11-25 16:52:51 +0530

    Add files via upload

---
+++ conditional statement/ifelsecondition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int hour;
+cout<< "Enter hour:";
+cin>>hour;
+if(hour>=9 && hour<=18){
+  cout<<"Working time";
+}
+else{
+  cout<<"leisure time";
+}
+return 0;
+}
