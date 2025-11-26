0d1182a86bbeb61b22d20503fc0cc67cf5320d4b
Anurag5 <anuraghere555@gmail.com>
2025-11-26 23:52:07 +0530

    Add files via upload

---
+++ conditional statement/Maxof2no.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int x,y;
+  cout<<"Enter 2 number:";
+  cin>>x>>y;
+
+
+  if(x>y){
+    cout<<"max is:"<<x;
+  }
+  else{
+    cout<<"max is:"<<y;
+  }
+  return 0;
+
+}
