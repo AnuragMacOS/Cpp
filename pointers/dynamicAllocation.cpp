c04a7ae723c0a845ac13efb4a5cddfb94f3d211f
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-16 21:39:52 +0530

    Add files via upload

---
+++ pointers/dynamicAllocation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int size;
+  cout<<"Enter number of elements:";
+  cin>>size;
+  int *p=new int[size];
+
+  cout<<"enter new size:";
+  cin>>size;
+  return 0;
+  }
